fe4a629f8b5c449594429f3e8cb6f04c4cd205e8
sonpapdi <snehalnakate4444@gamil.com>
2019-10-11 12:46:34 -0500

    object status

---
+++ practic/object.js
@@ -0,0 +1,14 @@
+let obj={
+    key:"values",
+    age:20,
+    colors:"pink",
+    dish:"sonpapdi"
+}
+objin:{
+    ram:"8GB"
+}
+
+
+console.log(obj.age)
+console.log(obj.colors)
+console.log(obj)
