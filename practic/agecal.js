5f6feb37c90869f7cdecbce1008458b85dd87bb5
sonpapdi <snehalnakate4444@gamil.com>
2019-10-13 03:28:31 -0500

    arithmatic

---
+++ practic/agecal.js
@@ -0,0 +1,14 @@
+function calculateAge (birthDate, otherDate) {
+    birthDate = new Date(birthDate);
+    otherDate = new Date(otherDate);
+
+    var years = (otherDate.getFullYear() - birthDate.getFullYear());
+
+    if (otherDate.getMonth() < birthDate.getMonth() || 
+        otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+        years--;
+    }
+
+    return years;
+console.log(calculateAge("12/10/1998","12/6/1899"))
+}
